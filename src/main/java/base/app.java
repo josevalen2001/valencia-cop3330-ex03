b88899787689d7d3c4b1b7cee6ffd405e406fb15
josevalen2001 <jose.juda.valencia@gmail.com>
2021-06-06 14:46:55 -0400

    Final Commit

--- src/main/java/base/app.java
+++ src/main/java/base/app.java
@@ -1,4 +1,27 @@
 package base;
+import java.util.*;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Jose Valencia
+ */
 
 public class app {
+
+    public static void main(String[] args) {
+        Scanner newScan = new Scanner(System.in);
+
+        //Input
+        System.out.print("What is the quote? ");
+        String quote = newScan.nextLine();
+
+        System.out.print("Who said it? ");
+        String author = newScan.nextLine();
+
+        //Concat""
+        String output = author + " says, \""+quote+"\"";
+
+        //Output
+        System.out.println(output);
+    }
 }
